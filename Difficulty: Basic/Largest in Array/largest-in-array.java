02ac4fc1c8edb2c8d87604332eaa7ab0ea0ddd2c
Arun-K007 <arunkrishnamoorthy324@gmail.com>
2026-07-09 17:13:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-in-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int largest(int[] arr) {
+        int sum= arr[0];
+        for(int i =0 ;i<arr.length;i++){
+            if (arr[i]>sum){
+                sum=arr[i];
+            }
+        }
+        return sum ;
+    }
+}
