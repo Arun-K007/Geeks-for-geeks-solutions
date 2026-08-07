c63a13a96af93446057feb359bd9046e60dc08aa
Arun-K007 <arunkrishnamoorthy324@gmail.com>
2026-08-07 15:15:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check Identical Matrices/check-identical-matrices.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean identicalMat(int[][] mat1, int[][] mat2) {
+        // code here
+        for(int i=0;i<mat1.length;i++){
+            for(int j=0;j<mat1[0].length;j++){
+                if(mat1[i][j]!=mat2[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
