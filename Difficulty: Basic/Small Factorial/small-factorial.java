0013343eda230c842ef2bfa18b4bba9b83cb58d6
Arun-K007 <arunkrishnamoorthy324@gmail.com>
2026-08-07 10:42:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Small Factorial/small-factorial.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long find_fact(int n) {
+        // Code here
+        long fact = 1;
+        for (int i= 1;i<=n;i++){
+            fact *=i;
+        }
+        return fact;
+    }
+}
